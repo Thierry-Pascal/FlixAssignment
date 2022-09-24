e45a211e3de3c8b12a5dbded52637332172d3638
Thierry-Pascal <thierryfleurant18@gmail.com>
2022-09-24 02:21:38 -0400

    added part 2

---
+++ FlixAssignment/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixAssignment
+//
+//  Created by Thierry-Pascal Fleurant on 9/24/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
